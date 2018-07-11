2e0fd843ae5a678711ba8ca01239353b781150f3
Jaime Radulovich <jradlov@gmail.com>
2018-07-11 08:00:45 -0700

    Add project files.

---
+++ Spinners_0/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+// Spinner items defined in strings.xml
+
+namespace Spinners_0
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner1);
+
+            ArrayAdapter adapter = ArrayAdapter.CreateFromResource(this,
+                Resource.Array.planets_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            spinner.Adapter = adapter;
+
+            spinner.ItemSelected += delegate {
+                Log.Debug("---debug---", "item selected: " + spinner.SelectedItem.ToString());
+            };
+        }
+    }
+}
+
